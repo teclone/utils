af4dce3f2f912b99d2e39bb60e399ce38fd5ba74
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2019-03-15 08:21:25 +0100

    feat: add more variable type testing functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,84 @@
+
+const toString = Object.prototype.toString;
+
 /**
  * tests if argument is a string
  */
 export const isString = (arg: any): arg is string => {
     return typeof arg === 'string';
-}
+};
+
+/**
+ * tests if argument is a number
+ */
+export const isNumber = (arg: any): arg is number => {
+    return typeof arg === 'number' && !isNaN(arg);
+};
+
+/**
+ * test if argument starts with integer, whether positive or negative
+ */
+export const isInt = (arg: any): arg is number => {
+    return /^[-+]?\d+/.test(arg);
+};
+
+/**
+ * test if argument starts with number(integer or float), whether positive or negative
+ */
+export const isNumeric = (arg: any): arg is number => {
+    return /^[-+.]?\d+/.test(arg);
+};
+
+/**
+ * test if argument is an array
+ */
+export const isArray = (arg: any): arg is Array<any> => {
+    return toString.call(arg) === '[object Array]' || arg instanceof Array;
+};
+
+/**
+ * test if argument is a function
+ */
+export const isCallable = (arg: any): arg is Function => {
+    return (toString.call(arg) === '[object Function]' || arg instanceof Function)
+        && !(arg instanceof RegExp);
+};
+
+/**
+ * test if argument is a regular expressions
+ */
+export const isRegex = (arg: any): arg is RegExp => {
+    return toString.call(arg) === '[object RegExp]' && arg instanceof RegExp;
+};
+
+/**
+ * test if argument is a javascript object
+ */
+export const isObject = (arg: any): arg is object => {
+    return typeof arg === 'object' && arg !== null;
+};
+
+/**
+ * test if argument is a plain javascript object
+ */
+export const isPlainObject = (arg: any): arg is object => {
+    if (isObject(arg)) {
+        const proto = Object.getPrototypeOf(arg);
+        return proto === null || proto === Object.getPrototypeOf({});
+    }
+    return false;
+};
+
+/**
+ * test if argument is a valid method parameter. by default, all types are valid parameters
+ * except undefined.
+ */
+export const isParameter = (arg: any, isNullValid: boolean = true) => {
+    if (typeof arg === 'undefined')
+        return false;
+
+    if (arg === null)
+        return isNullValid;
+
+    return true;
+};
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -9,4 +9,118 @@ describe('Utils', function() {
             expect(Utils.isString([])).toBeFalsy();
         });
     });
+
+    describe('.isNumber(variable)', function() {
+        it('should return true if argument is of type number and it is not NaN', function() {
+            expect(Utils.isNumber(22)).toBeTruthy();
+        });
+
+        it('should return false if argument is NaN or not of type number', function() {
+            expect(Utils.isNumber(NaN)).toBeFalsy();
+            expect(Utils.isNumber('22')).toBeFalsy();
+        });
+    });
+
+    describe('isInt(variable)', function() {
+        it(`should return true if argument is an int or can be cast to an int`, function() {
+            expect(Utils.isInt('042')).toBeTruthy();
+            expect(Utils.isInt('+33')).toBeTruthy();
+            expect(Utils.isInt('-004aaaa')).toBeTruthy();
+        });
+
+        it(`should return false if argument is not an int or can't be cast to an int`, function() {
+            expect(Utils.isInt('-+000.4eeeee')).toBeFalsy();
+            expect(Utils.isInt('.eeeee')).toBeFalsy();
+            expect(Utils.isInt('add.4eeeee')).toBeFalsy();
+        });
+    });
+
+    describe('isNumeric(variable)', function() {
+        it(`should return true if argument is a numeric value`, function() {
+            expect(Utils.isNumeric('000.4eeeee')).toBeTruthy();
+            expect(Utils.isNumeric('.0004eeeee')).toBeTruthy();
+            expect(Utils.isNumeric('+33.4eeeee')).toBeTruthy();
+            expect(Utils.isNumeric('-33.4eeeee')).toBeTruthy();
+        });
+
+        it(`should return false if argument is not a numeric value`, function() {
+            expect(Utils.isNumeric('-+000.4eeeee')).toBeFalsy();
+            expect(Utils.isNumeric('.eeeee')).toBeFalsy();
+            expect(Utils.isNumeric('add.4eeeee')).toBeFalsy();
+        });
+    });
+
+    describe('.isArray(variable)', function() {
+        it('should return true if argument is an array', function() {
+            expect(Utils.isArray([])).toBeTruthy();
+        });
+
+        it('should return false if argument is not an array', function() {
+            expect(Utils.isArray({})).toBeFalsy();
+            expect(Utils.isArray('')).toBeFalsy();
+        });
+    });
+
+    describe('.isCallable(variable)', function() {
+        it('should return true if argument is a function', function() {
+            expect(Utils.isCallable(name => name)).toBeTruthy();
+        });
+
+        it('should return false if argument is not a function', function() {
+            expect(Utils.isCallable(new RegExp('a'))).toBeFalsy();
+        });
+    });
+
+    describe('.isObject(variable)', function() {
+        it('should return true if argument is an object', function() {
+            expect(Utils.isObject({})).toBeTruthy();
+            expect(Utils.isObject([])).toBeTruthy();
+        });
+
+        it('should return false if argument is not an object', function() {
+            expect(Utils.isObject('')).toBeFalsy();
+            expect(Utils.isObject(null)).toBeFalsy();
+            expect(Utils.isObject(undefined)).toBeFalsy();
+        });
+    });
+
+    describe('.isPlainObject(variable)', function() {
+        it('should return true if argument is a plain javascript object', function() {
+            expect(Utils.isPlainObject({})).toBeTruthy();
+            expect(Utils.isPlainObject(Object.create(null))).toBeTruthy();
+        });
+
+        it('should return false if argument is not a plain object', function() {
+            expect(Utils.isPlainObject([])).toBeFalsy();
+            expect(Utils.isPlainObject(this)).toBeFalsy();
+            expect(Utils.isPlainObject('')).toBeFalsy();
+        });
+    });
+
+    describe('isRegex(variable)', function() {
+        it(`should return true if argument is a regex object`, function() {
+            expect(Utils.isRegex(/something/)).toBeTruthy();
+            expect(Utils.isRegex(new RegExp('^\\d+$'))).toBeTruthy();
+        });
+
+        it(`should return false if argument is not a regex object`, function() {
+            expect(Utils.isRegex('/something/')).toBeFalsy();
+            expect(Utils.isRegex({})).toBeFalsy();
+        });
+    });
+
+    describe('.isParameter(variable, isNullValid=true)', function() {
+        it('should return true if argument is a valid function parameter', function() {
+            expect(Utils.isParameter(3.2)).toBeTruthy();
+        });
+
+        it('should return false if argument is not a valid function parameter.', function() {
+            expect(Utils.isParameter(undefined)).toBeFalsy();
+        });
+
+        it('should accept a second boolean argument indicating if null arguments should be taken as valid', function() {
+            expect(Utils.isParameter(null, false)).toBeFalsy();
+            expect(Utils.isParameter(null)).toBeTruthy();
+        });
+    });
 });
