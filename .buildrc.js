08303797a50e821f4c402d073c16d9b04c25cacb
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2019-05-07 13:05:20 +0100

    fix: update build dependencies, change typings location

---
+++ .buildrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    srcDir: 'src',
+    mainModuleFileName: 'index.ts',
+    mainModuleName: 'Utils',
+    libConfig: {
+        enabled: true
+    },
+    distConfig: {
+        enabled: true,
+        uglify: true
+    }
+}
