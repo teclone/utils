f280e3fe35f2a9ed8a98bfd101c88ec42845593a
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2021-05-16 17:40:51 +0200

    fix: fixed get helper, return field if it is defined

--- src/helpers/get/index.ts
+++ src/helpers/get/index.ts
@@ -3,35 +3,36 @@ export const get = <T = any>(
   key: string | number | boolean,
   def?: T
 ): T => {
-  try {
-    const keys = key.toString().split('.');
+  if (obj) {
+    try {
+      const keys = key.toString().split('.');
 
-    let currentObj = obj;
-    let startIndex = -1;
+      let currentObj = obj;
+      let startIndex = -1;
 
-    while (++startIndex < keys.length - 1) {
-      currentObj = currentObj ? currentObj[keys[startIndex]] : undefined;
-    }
-
-    const lastKey = keys.pop();
-    if (Array.isArray(currentObj) && /^[+-]?\d+$/.test(lastKey)) {
-      const numberKey = parseInt(lastKey);
-
-      const multiplier: number = numberKey < 0 ? -1 : 1;
-      const absKey = Math.abs(numberKey);
-
-      if (currentObj.length > absKey) {
-        const value = currentObj[absKey];
-        if (typeof value === 'number') {
-          return (value * multiplier) as any;
-        } else {
-          return value as any;
-        }
+      while (++startIndex < keys.length - 1) {
+        currentObj = currentObj ? currentObj[keys[startIndex]] : undefined;
       }
-    } else if (currentObj) {
-      return currentObj[lastKey] || def;
-    }
-  } catch (ex) {}
 
+      const lastKey = keys.pop();
+      if (Array.isArray(currentObj) && /^[+-]?\d+$/.test(lastKey)) {
+        const numberKey = parseInt(lastKey);
+
+        const multiplier: number = numberKey < 0 ? -1 : 1;
+        const absKey = Math.abs(numberKey);
+
+        if (currentObj.length > absKey) {
+          const value = currentObj[absKey];
+          if (typeof value === 'number') {
+            return (value * multiplier) as any;
+          } else {
+            return value as any;
+          }
+        }
+      } else if (currentObj && typeof currentObj[lastKey] !== 'undefined') {
+        return currentObj[lastKey];
+      }
+    } catch (ex) {}
+  }
   return def;
 };
