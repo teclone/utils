31934538ae84f897e3f768c0b5f3947dfd021185
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2021-06-16 16:52:16 +0200

    doc: added documentation to hasRelatedAppInstalled method

--- src/browser/hasRelatedAppInstalled/index.ts
+++ src/browser/hasRelatedAppInstalled/index.ts
@@ -1,14 +1,16 @@
+import { isBrowser } from '../../helpers';
 import { getGlobal } from '../getGlobal';
 
 /**
- * checks if the application has related apps installed
+ * checks if the application has related apps installed,
+ * this method causes the browser to load your manifest json file
  * @param appPackageName
  */
 export const hasRelatedAppInstalled = (
   appPackageName: string
 ): Promise<boolean> => {
   const global = getGlobal();
-  if (global && 'getInstalledRelatedApps' in global.navigator) {
+  if (isBrowser && 'getInstalledRelatedApps' in global.navigator) {
     return global.navigator.getInstalledRelatedApps().then((results) => {
       return (
         results.length > 0 &&
--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ export * from './array';
 export * from './function';
 export * from './generators';
 export * from './browser';
-export { CASE_STYLES } from './constants';
+export * from './constants';
